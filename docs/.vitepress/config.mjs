91635f87993e62548dbeef9f4710a4a19105d1e0
Liang Wenshuo <93049773+AceYKN@users.noreply.github.com>
2025-12-12 12:18:30 +0800

    update HW

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -39,7 +39,9 @@ export default defineConfig({
             { text: '子环、理想与商环', link: '/math/abstract_algebra/subringideal' },
             { text: '子环、理想与商环 习题详解', link: '/math/abstract_algebra/subringidealHW' },
             { text: '环同态基本定理', link: '/math/abstract_algebra/ringhomomorphism' },
-            { text: '环同态基本定理 习题详解', link: '/math/abstract_algebra/ringhomomorphismHW' }
+            { text: '环同态基本定理 习题详解', link: '/math/abstract_algebra/ringhomomorphismHW' },
+            { text: '环的直积与中国剩余定理', link: '/math/abstract_algebra/ringdirectprod' },
+            { text: '环的直积与中国剩余定理 习题详解', link: '/math/abstract_algebra/ringdirectprodHW' }
           ]
         },
         {
